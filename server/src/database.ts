e7e6e2402e16de17b82aa59a08b33eb8e153780e
Luís Garcés <luisgarces@Luiss-MacBook-Pro.local>
2022-08-29 11:48:41 -0300

    Modify buttons Request PM in Speaker mode

--- server/src/database.ts
+++ server/src/database.ts
@@ -31,9 +31,6 @@ database.once('open', () => {
 		database.run(`INSERT OR IGNORE INTO "users" (username,password)
 	VALUES ("admin","admin")`);
 
-		database.run(`INSERT OR IGNORE INTO "users" (username,password)
-		VALUES ("admin","admin")`);
-
 		database.run(`CREATE TABLE IF NOT EXISTS "vote_motions" (
 	"id"	INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
 	"meeting"	INTEGER NOT NULL,
