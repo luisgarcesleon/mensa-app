192347da017142d5497444b71520ea935a2c4733
Luís Garcés <luisgarces@Luiss-MacBook-Pro.local>
2022-08-28 23:57:27 -0300

    Added sqlite3 update from ivica commits, placeholder delegates csv and added database fix insert

--- server/src/database.ts
+++ server/src/database.ts
@@ -29,6 +29,9 @@ database.once('open', () => {
      "password"	VARCHAR NOT NULL
 )`)
 
+		database.run(`INSERT OR IGNORE INTO "users" (username,password)
+		VALUES ("admin","admin")`);
+
 		database.run(`CREATE TABLE IF NOT EXISTS "vote_motions" (
 	"id"	INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
 	"meeting"	INTEGER NOT NULL,
